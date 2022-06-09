60d1cb7901d25f86e9fef504caace494fd75ce4c
MatiaPercic <mapercic@unipu.hr>
2022-06-09 12:08:35 +0200

    commit index

--- src/index.js
+++ src/index.js
@@ -1,10 +1,15 @@
 import express from "express";
 import cors from "cors";
+import { admins } from './admins';
+import { volonteri} from "./volonteri";
+import { voditeljskeAktivnosti } from "./voditeljskeAktivnosti";
+import {obliciRada} from "./obliciRada";
 
 const app=express();
 const port= 3000;
 
 //console.log('runs')
 
-app.use(cors());
-app.use(express.json());
+
+
+
