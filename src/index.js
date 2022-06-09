0d446e79e818603f809848073a2b7964143854e3
MatiaPercic <mapercic@unipu.hr>
2022-06-09 12:32:06 +0200

    cors

--- src/index.js
+++ src/index.js
@@ -1,1 +1,17 @@
-console.log("ok")
+import express from "express"
+import cors from "cors"
+
+
+const app= express();
+const port= 3001;
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req,res) => {
+    res.status(200)
+    res.send("ScoutHelp") }
+);
+
+
+app.listen(port, () => console.log(`Slušam na portu ${port}!`));
