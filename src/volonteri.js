6807ae014f97e35c7b9b1ce79dc47ffa211643b0
MatiaPercic <mapercic@unipu.hr>
2022-06-09 12:34:27 +0200

    added volonteri.js

---
+++ src/volonteri.js
@@ -0,0 +1,25 @@
+export const volonteri = [
+    {
+        id: '0001',
+        ime: 'Marko',
+        prezime: 'Marković',
+        godine: 20
+
+    },
+
+    {
+        id: '0002',
+        ime: 'Niko',
+        prezime: 'Nikolić',
+        godine: 23
+    },
+
+    {
+        id: '0003',
+        ime: 'Olga',
+        prezime: 'Olić',
+        godine: 21
+    },
+];
+
+
